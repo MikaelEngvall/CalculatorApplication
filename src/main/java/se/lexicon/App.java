f972567eff4e0efb010cad13bedfb816c9246a0e
MikaelEngvall <mikael@engvall.org>
2023-07-08 15:36:00 +0200

    Added a clearScreen method to the application. Works only with Windows OS.

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -4,6 +4,8 @@
  */
 
 package se.lexicon;
+
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
@@ -12,6 +14,7 @@ public class App {
         boolean exit = false;
 
         while (!exit) {
+            clearScreen();
             System.out.println("Calculator Application");
             System.out.println("Please enter the first number: ");
             double num1 = scanner.nextDouble();
@@ -34,12 +37,26 @@ public class App {
             System.out.println("Do you want to perform another calculation? (Y/N)");
             String choice = scanner.next();
             if (choice.equalsIgnoreCase("N")) {
+                clearScreen();
                 exit = true;
             }
         }
 
         System.out.println("Calculator Application closed.");
         scanner.close();  // When I read documentation on Scanner it refers often to the fact that scanner should be closed after use.
+
+    }
+
+    // Method to clear the screen
+    private static void clearScreen() {
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
 
